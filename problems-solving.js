99f5b1fbd77caee62f0529f86c13d5d155a326c0
parthadas01746 <parthadas01746@gmail.com>
2021-08-18 19:40:19 +0600

    first-commit

--- problems-solving.js
+++ problems-solving.js
@@ -1,13 +1,6 @@
-// got to another page by clicking the button
-document.getElementById("sumbit-btn").addEventListener('click', function(){
-    var emailfield = document.getElementById("email-field");
-    var email = emailfield.value;
-    var passwordofield = document.getElementById("passwordo-field");
-    var password = passwordofield.value;
-    if(email =="partha@gmail.com" && password ==12345){
-        window.location.href = "bank.html";
-    }
-    else(alert("please enter valid email & password"));
-
-    
+document.getElementById("Moon-btn").addEventListener("click", function(){
+   var moonInput = document.getElementById("moon-input");
+   var moonInputValue = parseInt(moonInput.value); 
+    var moonInputValue = moonInputValue +1;
+    console.log(moonInputValue)
 })
